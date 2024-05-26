38679b692ba80dd7fb89f11faacb9eaea96f4dfb
Mazen Hassan <95134348+Mazz100@users.noreply.github.com>
2024-05-27 01:03:12 +0300

    Style update and README.md

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,21 @@ function App() {
         <TestimonialCarousel />
       </main>
 
-      <footer></footer>
+      <footer className="attribution">
+        Challenge by{" "}
+        <a
+          className="footer-links"
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a className="footer-links" href="https://github.com/Mazz100">
+          Mazen Hassan
+        </a>
+        .
+      </footer>
     </>
   );
 }
